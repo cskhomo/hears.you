c452ef6f29f29d52694ac936134db0a2a8e70aee
Sizwe Chris Khomo <cs.khomo@outlook.com>
2026-03-29 23:05:19 +0200

    assign menu icons

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
-import sys
+from sys import argv
+from sys import exit
 
 from PyQt6.QtWidgets import QMainWindow
 from PyQt6.QtWidgets import QTextEdit
 from PyQt6.QtGui import QAction
+from PyQt6.QtGui import QIcon
 from PyQt6.QtWidgets import QApplication
 
 
 from PyQt6.QtWidgets import QToolBar
-from PyQt6.QtGui import QIcon
 
 
 class MainWindow(QMainWindow):
     
-    def __init__(self):
+    def __init__(self, app):
         super().__init__()
+
+	#icons file location
         
         self.setWindowTitle("Hears")
         self.setGeometry(100, 100, 600, 600)
@@ -24,29 +27,29 @@ class MainWindow(QMainWindow):
 
         file_menu = menu.addMenu("&File")
 
-        new_action = QAction("New", self)
+        new_action = QAction(QIcon("./assets/icons/menu/new.png"),"New", self)
         new_action.setShortcut("CTRL+N")
         new_action.triggered.connect(self.new)
         file_menu.addAction(new_action)
 
-        open_action = QAction("Open", self)
+        open_action = QAction(QIcon("./assets/icons/menu/open.png"), "Open", self)
         open_action.setShortcut("CTRL+O")
         open_action.triggered.connect(self.open)
         file_menu.addAction(open_action)
 
-        save_action = QAction("Save", self)
+        save_action = QAction(QIcon("./assets/icons/menu/save.png"),"Save", self)
         save_action.setShortcut("CTRL+S")
         save_action.triggered.connect(self.save)
         file_menu.addAction(save_action)
 
         file_menu.addSeparator()
 
-        close_action = QAction("Close", self)
+        close_action = QAction(QIcon("./assets/icons/menu/close.png"),"Close", self)
         close_action.setShortcut("CTRL+W")
         close_action.triggered.connect(self.close)
         file_menu.addAction(close_action)
         
-        quit_action = QAction("Quit", self)
+        quit_action = QAction(QIcon("./assets/icons/menu/quit.png"), "Quit", self)
         quit_action.setShortcut("CTRL+Q")
         quit_action.triggered.connect(self.quit)
         file_menu.addAction(quit_action)
@@ -54,29 +57,29 @@ class MainWindow(QMainWindow):
 
         edit_menu = menu.addMenu("&Edit")
 
-        undo_action = QAction("Undo", self)
+        undo_action = QAction(QIcon("./assets/icons/menu/undo.png"),"Undo", self)
         undo_action.setShortcut("CTRL+Z")
         undo_action.triggered.connect(self.text_edit.undo)
         edit_menu.addAction(undo_action)
 
-        redo_action = QAction("Redo", self)
+        redo_action = QAction(QIcon("./assets/icons/menu/redo.png"),"Redo", self)
         redo_action.setShortcut("CTRL+Y")
         redo_action.triggered.connect(self.text_edit.redo)
         edit_menu.addAction(redo_action)
 
         edit_menu.addSeparator()
 
-        cut_action = QAction("Cut", self)
+        cut_action = QAction(QIcon("./assets/icons/menu/cut.png"),"Cut", self)
         cut_action.setShortcut("CTRL+X")
         cut_action.triggered.connect(self.text_edit.cut)
         edit_menu.addAction(cut_action)
 
-        copy_action = QAction("Copy", self)
+        copy_action = QAction(QIcon("./assets/icons/menu/copy.png"),"Copy", self)
         copy_action.setShortcut("CTRL+C")
         copy_action.triggered.connect(self.text_edit.copy)
         edit_menu.addAction(copy_action)
 
-        paste_action = QAction("Paste", self)
+        paste_action = QAction(QIcon("./assets/icons/menu/paste.png"),"Paste", self)
         paste_action.setShortcut("CTRL+V")
         paste_action.triggered.connect(self.text_edit.paste)
         edit_menu.addAction(paste_action)
@@ -96,11 +99,12 @@ class MainWindow(QMainWindow):
         self.text_edit.clear()
 
     def quit(self):
-        self.destroy()  
-     
+        self.destroy()
+        app.exit()
+    
     
 if __name__ == "__main__":
     
-    app = QApplication(sys.argv)
-    window = MainWindow()
-    sys.exit(app.exec())
+    app = QApplication(argv)
+    window = MainWindow(app)
+    exit(app.exec())
